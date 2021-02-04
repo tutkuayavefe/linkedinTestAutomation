066ebf50de6fcf13873b0184391be0699e2ea694
tutkuayavefe <tutkuayavefe@gmail.com>
2021-02-04 16:29:43 +0100

    test methods have been added (Add skill tests)

--- src/main/java/com/linkedin/pages/LinkedinPage.java
+++ src/main/java/com/linkedin/pages/LinkedinPage.java
@@ -1,62 +1,74 @@
 package com.linkedin.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LinkedinPage {
 
-	
 	private String emailField = "username";
 	private String password = "password";
-	private String userEmail = "tutkuayavefe@gmail.com";
-	private String userPassword = "***,";
-	private String signInButton = "button";
+	private String userEmail = "testemailforautomationcases@gmail.com";
+	private String userPassword = "Testautomationcases159.";
+	private String signInButton = "//button[contains(@data-litms-control-urn,'login-submit')]";
 	private String logoMe = "ember35";
-	private String viewProfile = "ember37";
-	private String addSkill = "ember558";
-
+	private String viewProfile = "//a[contains(.,'View Profile')]";
+	private String addSkill = "//a[contains(@data-control-name,'edit_skills_add')]";
+	private String addSkillInput = "//input[contains(@aria-label,'Skill (ex: Data Analysis)')]";
+	private String addButton = "//button[@class='artdeco-button ml2 fr ember-view'][contains(.,'Add')]";
+	private String skiilYouWantToAdd = "Linkedin add skill";
+	private String addedSkill = "//span[contains(.,'Linkedin add skill')]";
+	
 	
 
 	public LinkedinPage(WebDriver driver) {
 		// TODO Auto-generated constructor stub
 	}
-	
-	
+
 	public String getEmailField() {
 		return emailField;
 	}
+
 	public String getPasswordField() {
 		return password;
 	}
-
 
 	public String getUserEmail() {
 		return userEmail;
 	}
 
-
 	public String getUserPassword() {
 		return userPassword;
 	}
-	
+
 	public String getSignInButton() {
 		return signInButton;
 	}
-
 
 	public String getLogoMe() {
 		return logoMe;
 	}
 
-
 	public String getViewProfile() {
 		return viewProfile;
 	}
-	
 
 	public String getAddSkill() {
 		return addSkill;
 	}
 
+	public String getAddSkillInput() {
+		return addSkillInput;
+
+	}
+
+	public String getAddButton() {
+		return addButton;
+	}
+
+	public String getSkiilYouWantToAdd() {
+		return skiilYouWantToAdd;
+	}
 	
+	public String getAddedSkill() {
+		return addedSkill;
+	}
 }
--- src/test/java/com/linkedin/test/AddSkillTest.java
+++ src/test/java/com/linkedin/test/AddSkillTest.java
@@ -2,24 +2,19 @@ package com.linkedin.test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import com.linkedin.base.BaseTest;
 import com.linkedin.pages.LinkedinPage;
 
+import net.bytebuddy.dynamic.scaffold.MethodGraph.Linked;
+
 public class AddSkillTest extends BaseTest {
 
 	LinkedinPage linkedinPage;
 
-//	@Test(priority = 1)
-//	public void  navigationToLinkedin() {
-//		// This test case to verify if navigation is correct or not
-//		System.out.println("Linkedin page opened. Test passed!");
-//		String title = driver.getTitle();
-//		System.out.println(title);
-//		Assert.assertEquals(title, "Linkedin");
-//	}
 
 	@Test(priority = 1)
 	public void loginToLinkedinWithExistAccount() {
@@ -34,32 +29,72 @@ public class AddSkillTest extends BaseTest {
 		passwordField.click();
 		passwordField.sendKeys(linkedinPage.getUserPassword());
 
-		WebElement signInButton = driver.findElement(By.xpath("//*[contains(text(), 'Oturum açın')]"));
+		WebElement signInButton = driver.findElement(By.xpath(linkedinPage.getSignInButton()));
 
-		// driver.findElement(By.xpath("//input[@type='submit']"));
+		if (signInButton.isDisplayed()) {
+			signInButton.click();
 
-		signInButton.click();
-		
+		}
+
 	}
 
 	@Test(priority = 2)
-	public void navigationToProfile() {
+	public void navigationToProfile() throws InterruptedException {
 		loginToLinkedinWithExistAccount();
 		linkedinPage = new LinkedinPage(driver);
 
+		Thread.sleep(1000);
+
 		WebElement logoMe = driver.findElement(By.id(linkedinPage.getLogoMe()));
 		logoMe.click();
-		
-		WebElement viewProfile = driver.findElement(By.xpath("//*[contains(text(), 'View Profile')]"));
 
-		viewProfile.click();
-		
-	//	WebElement addSkillButtonText = driver.findElement(By.id(linkedinPage.getAddSkill()));
-		//addSkillButtonText.sendKeys(Keys.DOWN);
-	
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("window.scrollBy(0,5000)");
-		
+		WebElement viewProfile = driver.findElement(By.xpath(linkedinPage.getViewProfile()));
+
+		if (viewProfile.isDisplayed()) {
+			viewProfile.click();
+
+		}
+
+	}
+
+	@Test(priority = 3)
+	public void scrollDownToAddSkillArea() throws InterruptedException {
+		navigationToProfile();
+		Thread.sleep(3000);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,1000)");
+		Thread.sleep(2000);
+		js.executeScript("window.scrollBy(0,1000)");
+		Thread.sleep(1000);
+
+		WebElement addSkill = driver.findElement(By.xpath(linkedinPage.getAddSkill()));
+		if (addSkill.isDisplayed()) {
+			addSkill.click();
+
+		}
+
+	}
+
+	@Test(priority = 4)
+
+	public void addingSkill() throws InterruptedException {
+		scrollDownToAddSkillArea();
+		WebElement addSkill = driver.findElement(By.xpath(linkedinPage.getAddSkill()));
+		addSkill.click();
+		Thread.sleep(1000);
+
+		WebElement addSkillInput = driver.findElement(By.xpath(linkedinPage.getAddSkillInput()));
+		addSkillInput.click();
+		addSkillInput.sendKeys(linkedinPage.getSkiilYouWantToAdd());
+		addSkillInput.sendKeys(Keys.ENTER);
+		Thread.sleep(2000);
+		WebElement addSkillButton = driver.findElement(By.xpath(linkedinPage.getAddButton()));
+		addSkillButton.click();
+
+		WebElement addedSkill = driver.findElement(By.xpath(linkedinPage.getAddedSkill()));
+		if(addedSkill.isDisplayed()) {
+			System.out.println("Done!!!");
+		}
 	}
 
 }
